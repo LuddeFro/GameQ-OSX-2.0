56532b11159e406480f6d87a31ea9d185e82225b
LuddeFro <ludvig.froberg@gmail.com>
2015-05-18 12:55:01 +0200

    fixed file moving

---
+++ GameQ-OSX-2.0/GameQ-OSX-2.0-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+#import <pcap.h>
+#import <string.h>
+#import <stdlib.h>
+#include <stdio.h>
+#include <ifaddrs.h>
+
+
+#import <netinet/ip.h>
+#import <netinet/udp.h>
+#import <netinet/in.h>
+#import <arpa/inet.h>
+#import <net/ethernet.h>
+
