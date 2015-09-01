7f0fdf7e1bca84963c4f7a52184a10fd1af5edd7
fabian.wikstrom <fabian.wikstrom@gmail.com>
2015-09-01 14:25:15 -0400

    texts

--- GameQ-OSX-2.0/GameStatus.swift
+++ GameQ-OSX-2.0/GameStatus.swift
@@ -60,8 +60,8 @@ class Encoding {
             switch status{
             case .Offline: return "Offline"
             case .Online: return  "Online"
-            case .InLobby: return "Finding Match"
-            case .InQueue: return "Finding Match"
+            case .InLobby: return "Detecting Game"
+            case .InQueue: return "Detecting Game"
             case .GameReady: return "Your Match is Ready"
             case .InGame:return "In Match"
             }
@@ -69,8 +69,8 @@ class Encoding {
             switch status{
             case .Offline: return "Offline"
             case .Online: return  "Online"
-            case .InLobby: return "Finding Match"
-            case .InQueue: return "Finding Match"
+            case .InLobby: return "Detecting Game"
+            case .InQueue: return "Detecting Game"
             case .GameReady: return "Your Match is Ready"
             case .InGame:return "In Match"
             }
@@ -78,8 +78,8 @@ class Encoding {
             switch status{
             case .Offline: return "Offline"
             case .Online: return  "Online"
-            case .InLobby: return "Detecting Match"
-            case .InQueue: return "Detecting Match"
+            case .InLobby: return "Detecting Game"
+            case .InQueue: return "Detecting Game"
             case .GameReady: return "Your Match is Ready"
             case .InGame:return "In Match"
             }
@@ -87,8 +87,8 @@ class Encoding {
             switch status{
             case .Offline: return "Offline"
             case .Online: return  "Online"
-            case .InLobby: return "Finding Match"
-            case .InQueue: return "Finding Match"
+            case .InLobby: return "Detecting Game"
+            case .InQueue: return "Detecting Game"
             case .GameReady: return "Your Match is Ready"
             case .InGame:return "In Match"
             }
@@ -96,8 +96,8 @@ class Encoding {
             switch status{
             case .Offline: return "Offline"
             case .Online: return  "Online"
-            case .InLobby: return "Searching for Match"
-            case .InQueue: return "Searching for Match"
+            case .InLobby: return "Detecting Game"
+            case .InQueue: return "Detecting Game"
             case .GameReady: return "Your Match is Ready"
             case .InGame:return "In Match"
             }
